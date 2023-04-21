d3633e5da92dfec79acee3b34ea96a53164e7962
jambrizz <ambrizjovani@gmail.com>
2023-04-21 00:39:50 -0700

    Prep1 & Prep2 complete

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Clear();
+        Console.Write("What is your First Name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is yout Last Name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Clear();
+        Console.Write("What was your percentage grade? ");
+        string input = Console.ReadLine();
+        bool inputAInt = int.TryParse(input, out int grade);
+
+        if(inputAInt == false)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Please enter a valid number.");
+            input = Console.ReadLine();
+            inputAInt = int.TryParse(input, out grade);
+        }
+
+        string letter = "";
+
+        if(grade >= 90)
+        {
+            letter = "A";
+        }
+        else if(grade >= 80 && grade < 90)
+        {
+            letter = "B";
+        }
+        else if(grade >= 70 && grade < 80)
+        {
+            letter = "C";
+        }
+        else if(grade >= 60 && grade < 70)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}.");
+
+        if(grade >= 70)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Congrats! You passed the course!");
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine("Sorry, you did not pass the course.");
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
