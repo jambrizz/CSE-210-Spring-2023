22be3c7c9285fb8fcab62b163798ea516841139b
jambrizz <ambrizjovani@gmail.com>
2023-04-25 22:34:53 -0700

    Prep 3 complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
@@ -6,42 +8,38 @@ class Program
     {
         Console.Clear();
         int magicNumber = new Random().Next(1, 100);
-        Console.WriteLine($"{magicNumber}");
         Console.WriteLine();
+        Console.WriteLine("I'm thinking of a number between 1 and 100.");
 
         bool keepGuessing = true;
+        int count = 0;
 
         do
         {
-            int count = 0;
             Console.Write("What is your guess? ");
             string guess = Console.ReadLine();
             bool isNumber = int.TryParse(guess, out int guessNumber);
+            count++;
 
             if (isNumber == false)
             {
-                Console.WriteLine("Invalid input! Please enter a number.");
                 Console.WriteLine();
-                Console.Write("What is your guess? ");
+                Console.Write("That is not a number! Try again.");
                 guess = Console.ReadLine();
                 isNumber = int.TryParse(guess, out guessNumber);
             }
 
-            if (guessNumber < magicNumber)
+            if(guessNumber < magicNumber)
             {
-                Console.WriteLine("Higher!");
-                count++;
+                Console.WriteLine("Higher");
             }
-            else if (guessNumber > magicNumber)
+            else if(guessNumber > magicNumber)
             {
-                Console.WriteLine("Lower!");
-                count++;
+                Console.WriteLine("Lower");
             }
             else
             {
                 Console.WriteLine("You got it!");
-                count++;
-                ///////////TODO fix the count issue//////////////////
                 if(count == 1)
                 {
                     Console.WriteLine($"It took you {count} guess.");
@@ -50,39 +48,29 @@ class Program
                 {
                     Console.WriteLine($"It took you {count} guesses.");
                 }
+                //keepGuessing = false;
                 Console.WriteLine();
-                Console.Write("Would you like to play again? (y/n) ");
-                string playAgain = Console.ReadLine();
-                if (playAgain == "y" || playAgain == "Y")
-                {
-                    Console.Clear();
-                    magicNumber = new Random().Next(1, 100);
-                    Console.WriteLine($"{magicNumber}");
-                    Console.WriteLine();
-                }
-                else if(playAgain == "n" || playAgain == "N")
+                Console.WriteLine("Do you want to keep playing? (y/n)");
+                string answer = Console.ReadLine().ToLower();
+
+                if(answer == "n")
                 {
                     keepGuessing = false;
+                    Console.WriteLine("Thank you for playing! Goodbye.");
+                }
+                else if(answer == "y")
+                {
+                    Console.Clear();
+                    count = 0;
+                    magicNumber = new Random().Next(1, 100);
+                    Console.WriteLine("I'm thinking of a number between 1 and 100.");
                 }
                 else
                 {
-                    Console.WriteLine("Invalid input! Please enter y or n.");
-                    Console.WriteLine();
-                    Console.Write("Would you like to play again? (y/n) ");
-                    playAgain = Console.ReadLine();
-                    if (playAgain == "y" || playAgain == "Y")
-                    {
-                        Console.Clear();
-                        magicNumber = new Random().Next(1, 100);
-                        Console.WriteLine($"{magicNumber}");
-                        Console.WriteLine();
-                    }
-                    else if (playAgain == "n" || playAgain == "N")
-                    {
-                        keepGuessing = false;
-                    }
+                    Console.WriteLine("That is not a valid answer. Please type 'y' for yes or 'n' for no.");
+                    answer = Console.ReadLine().ToLower();
                 }
-            }
+            } 
         }
         while (keepGuessing == true);
         
