847d6d0e47f05bf777cce027e204f0fa412dc41e
jambrizz <ambrizjovani@gmail.com>
2023-04-26 00:04:12 -0700

    prep5 draft

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -6,4 +6,29 @@ class Program
     {
         Console.WriteLine("Hello Prep5 World!");
     }
+
+    static void DisplayWelcomeMessage()
+    {
+
+    }
+
+    static string PromptUserName()
+    {
+        return "";
+    }
+
+    static int PromptUserNumber()
+    {
+        return 0;
+    }
+
+    static int SquareNumber(int number)
+    {
+        return 0;
+    }
+
+    static string DisplayResult()
+    {
+        return "";
+    }
 }
