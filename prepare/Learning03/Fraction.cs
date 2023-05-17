6ab70af301695df02956566970d84593010b64aa
jambrizz <ambrizjovani@gmail.com>
2023-05-17 00:09:17 -0700

    W05 learning activity complete

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string result = $"{_top}/{_bottom}";
+        return result;
+    }
+
+    public double GetDecimalValue()
+    {
+        double result = (double)_top / (double)_bottom;
+        return result; 
+    }
+}
