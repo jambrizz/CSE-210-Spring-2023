7b8fb988811c67a0aa1eeb204d006e462817e693
jambrizz <ambrizjovani@gmail.com>
2023-05-24 00:53:33 -0700

    Week 6 prove Reference Class created

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -4,5 +4,53 @@ using System.Linq;
 
 public class Reference
 {
-    
+    public string _book;
+
+    public int _chapter;
+
+    public int _verse;
+
+    public Reference(string book, int chapter, int verse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+    }
+
+    public void SetBook(string book)
+    {
+        _book = book;
+    }
+
+    public void SetChapter(int chapter)
+    {
+        _chapter = chapter;
+    }
+
+    public void SetVerse(int verse)
+    {
+        _verse = verse;
+    }
+
+    public string GetBook()
+    {
+        return _book;
+    }
+
+    public int GetChapter()
+    {
+        return _chapter;
+    }
+
+    public int GetVerse()
+    {
+        return _verse;
+    }
+
+    public string GetReference()
+    {
+        return $"{_book} {_chapter}:{_verse}";
+    }
+
+
 }
