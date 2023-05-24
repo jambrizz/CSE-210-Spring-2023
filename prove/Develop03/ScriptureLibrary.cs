25880dffeae4d5c4d0bdaebd48c394b07ef90b1f
jambrizz <ambrizjovani@gmail.com>
2023-05-23 23:42:33 -0700

    Week 6 prove ScriptureLibrary class created

--- prove/Develop03/ScriptureLibrary.cs
+++ prove/Develop03/ScriptureLibrary.cs
@@ -7,8 +7,25 @@ public class ScriptureLibrary
     private int _random;
     private List<Scriptures> _scriptures = new List<Scriptures>();
 
+    public ScriptureLibrary()
+    {
+        
+    }
+
     public string GetRandomScripture()
     {
         return "";
     }
+
+    public void LoadScripturesFromFiles()
+    {
+
+    }
+
+    private string ParseScriptureReference(string scripture)
+    {
+        return "";
+    }
+
+   
 }
