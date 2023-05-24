a3f1f952a797d57e096b5017bc1c37aae109dc3b
jambrizz <ambrizjovani@gmail.com>
2023-05-23 22:32:43 -0700

    Week 6 Prove menu feature created

---
+++ prove/Develop03/ScriptureLibrary.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ScriptureLibrary
+{
+    private int _random;
+    private List<Scriptures> _scriptures = new List<Scriptures>();
+
+    public string GetRandomScripture()
+    {
+        return "";
+    }
+}
