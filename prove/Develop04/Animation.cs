1308b8398c3dab763a25b34db17fe49d8682ea3a
jambrizz <ambrizjovani@gmail.com>
2023-06-04 21:16:02 -0700

    Week8 prove Animation class complete

--- prove/Develop04/Animation.cs
+++ prove/Develop04/Animation.cs
@@ -5,5 +5,33 @@ using System.Text;
 
 public class Animation
 {
-    
+    List<string> _animation = new List<string>()
+    {
+        "|",
+        "/",
+        "-",
+        "\\",
+        "|",
+        "/",
+        "-",
+        "\\",
+        "|",
+        "/",
+        "-",
+        "\\",
+        "|",
+        "/",
+        "-",
+        "\\"
+    };
+
+    public void AnimationDisplay()
+    {
+        foreach (string item in _animation)
+        {
+            Console.Write(item);
+            System.Threading.Thread.Sleep(200);
+            Console.Clear();
+        }
+    }
 }
