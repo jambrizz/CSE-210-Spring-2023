8b837fbc8835809b4c5595b9b8717316b1dac9e9
jambrizz <ambrizjovani@gmail.com>
2023-06-08 00:59:25 -0700

    Week8 Prove Animation class update1

--- prove/Develop04/Animation.cs
+++ prove/Develop04/Animation.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
 
 public class Animation
 {
@@ -25,15 +27,20 @@ public class Animation
         "\\"
     };
 
-    //TODO: fix animation display below it curently does not match the video explanation
+    //TODO: add parameters to the AnimationDisplay methods to allow for custom cursor positions
     public void AnimationDisplayStandard()
     {
+        //CursorVisible hides the cursor
+        CursorVisible = false;
         foreach (string item in _animation)
         {
+            //SetCursorPosition sets the cursor position to the specified coordinates (x, y)
+            SetCursorPosition(0, 3);
             Console.Write(item);
-            System.Threading.Thread.Sleep(500);
-            Console.Clear();
+            Thread.Sleep(200);
         }
+        //CursorVisible shows the cursor after the animation is done
+        CursorVisible = true;
     }
 
     public void AnimationDisplayCustom(int time)
@@ -43,7 +50,7 @@ public class Animation
             foreach (string item in _animation)
             {
                 Console.Write(item);
-                System.Threading.Thread.Sleep(200);
+                Thread.Sleep(200);
                 Console.Clear();
             }
             time--;
