255650428b47f6ccbd51192842d1342fc470a1af
jambrizz <ambrizjovani@gmail.com>
2023-06-14 00:16:05 -0700

    Week9 Learning 05 Polymorphism

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("Red", 5);
+        shapes.Add(square);
+
+        Circle circle = new Circle("Blue", 3);
+        shapes.Add(circle);
+
+        Rectangle rectangle = new Rectangle("Green", 4, 5);
+        shapes.Add(rectangle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor() + " " + shape.GetArea());
+        }
     }
 }
