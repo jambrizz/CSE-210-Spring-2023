12baf4022d136a164398c948d521de28a80487c6
jambrizz <ambrizjovani@gmail.com>
2023-07-04 22:56:01 -0700

    Final: Classes created

---
+++ final/FinalProject/Quest.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Quest
+{
+    private bool _heroAlive;
+
+    private bool _monsterAlive;
+
+    private List<int> questionsAsked;
+
+    private List<int> storyLinesDisplayed;
+
+    public void QuestApp()
+    {
+
+    }
+
+    public string DisplayHeroStats()
+    {
+        return "";
+    }
+
+    public string DisplayMonsterStats()
+    {
+        return "";
+    }
+
+    public bool HeroDefeated()
+    {
+        // TODO replace this with a real implementation
+        return false;
+    }
+
+    public bool MonsterDefeated()
+    {
+        // TODO replace this with a real implementation
+        return false;
+    }
+
+    public bool PathComplete()
+    {
+        // TODO replace this with a real implementation
+        return false;
+    }
+
+    public bool QuestComplete()
+    {
+        // TODO replace this with a real implementation
+        return false;
+    }
+}
